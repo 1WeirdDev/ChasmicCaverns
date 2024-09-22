296cd7b645a37a9019c86f3ff42206df44fd87f8
The1WeirdDev <the1weirddev@gmail.com>
2024-09-21 20:54:53 -0500

    Update

--- src/Client/Entity/Player.cpp
+++ src/Client/Entity/Player.cpp
@@ -6,8 +6,6 @@
 #include "Math/MatrixUtils.h"
 #include "Game.h"
 
-#include <glm/gtc/matrix_transform.hpp>
-
 void Player::Init(){
 
 }
@@ -27,7 +25,7 @@ void Player::Update(){
         moveDirection.x -= (float)sin(m_Rotation.y) * Time::GetDeltaTime();
         moveDirection.z += (float)cos(m_Rotation.y) * Time::GetDeltaTime();
     }
-    
+
     if(Input::IskeyDown(GLFW_KEY_A)){
         moveDirection.x -= (float)sin(m_Rotation.y - Mathf::ToRadians(90.0f)) * Time::GetDeltaTime();
         moveDirection.z += (float)cos(m_Rotation.y - Mathf::ToRadians(90.0f)) * Time::GetDeltaTime();
@@ -42,8 +40,6 @@ void Player::Update(){
     Game::GetShader().Start();
     m_ViewMatrix.SetIdentity();
     
-    glm::mat4x4 test;
-    glm::scale(test, glm::vec3(1, 1, 1));
     MatrixUtils::RotateMat4x4(m_ViewMatrix.GetData(), m_Rotation.z, Vec3<float>(1.0f, 0.0f, 0.0f));
     MatrixUtils::RotateMat4x4(m_ViewMatrix.GetData(), m_Rotation.y, Vec3<float>(0.0f, 1.0f, 0.0f));
     MatrixUtils::TranslateMat4x4<float>(m_ViewMatrix.GetData(), m_Position);
