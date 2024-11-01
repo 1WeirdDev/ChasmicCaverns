6aac4fa5d4b56024f237e65b88077f3a95ca4642
The1WeirdDev <the1weirddev@gmail.com>
2024-11-01 10:32:17 -0500

    Update

--- src/Client/Rendering/Mesh/MeshData.h
+++ src/Client/Rendering/Mesh/MeshData.h
@@ -1,5 +1,6 @@
 #pragma once
 
+//Types are assigned in this order to have specific values for easy addition with GLFW
 enum VertexType{
     VT_INT8 = 0,
     VT_UINT8,
@@ -16,7 +17,7 @@ enum VertexType{
 enum IndexType{
     FT_UINT8 = 0,
 
-    FT_UINT16,
+    FT_UINT16 = 2,
 
-    FT_UINT32,
+    FT_UINT32 = 4,
 };
